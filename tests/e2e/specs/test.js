c78830029e3e6383126be33273e4ca3177e8242a
Luiz Filipe Moura <filipemoura202@gmail.com>
2022-03-21 16:53:53 -0300

    fix error test e2e

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,8 +3,7 @@ const APP_URL = process.env.APP_URL || 'http://localhost:8080'
 describe('Home', () => {
   it('Visits the main page and should render a standy by button', () => {
     cy.visit(APP_URL)
-    cy.wait(900)
-    cy.get('#fisrt-div-playground')
+    cy.wait(200)
     cy.get('#standy-button')
   })
 })
